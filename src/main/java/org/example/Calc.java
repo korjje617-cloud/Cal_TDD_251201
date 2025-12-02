316ddc59a821591016746d40666efdc4697f1756
korjje617 <korjje617@gmail.com>
2025-12-02 16:25:09 +0900

    이중괄호 제거까지

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -465,7 +465,8 @@ public class Calc {
         return 0;
     }
 }
-     */
+
+============== v9 강사님 풀이  ==============
 
     public static int run(String exp) {
         // 괄호 제거
@@ -525,4 +526,66 @@ public class Calc {
         return exp;
     }
 
+}
+     */
+
+    public static int run(String exp) {
+        // 괄호 제거
+        exp = stripOuterBrakets(exp);
+
+        // 숫자만 들어올 경우 바로 리턴
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
+        }
+
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+
+        boolean needToCompound = needToPlus && needToMulti;
+
+        exp = exp.replace("- ", "+ -");
+
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        }
+
+        if (needToPlus) {
+            String[] bits = exp.split(" \\+ ");
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+    }
+
+    private static String stripOuterBrakets(String exp) {
+        if (exp.contains("((")) {
+            exp = exp.substring(2, exp.length() - 2);
+        } else if (exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')') {
+            exp = exp.substring(1, exp.length() - 1);
+
+        }
+        return exp;
+    }
 }
